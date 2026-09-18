7918041a5f4b733c6933acaaed874a17b1c6c5c8
Jose <joseernestoroldan@gmail.com>
2026-09-18 11:50:41 -0400

    feat(donate): add donate button component and carousel images
    - Add DonateButton component with hover/focus styles
    - Add remaining carousel images (image04, image05)

---
+++ components/Navbar/DonateButton/DonateButton.tsx
@@ -0,0 +1,11 @@
+import styles from "./DonateButton.module.css";
+
+const DonateButton = () => {
+  return (
+    <button className={styles.donateButton} type="button">
+      Donate
+    </button>
+  );
+};
+
+export default DonateButton;
